11d999044d2d7d3ca57d275b8f22b06ca99aa988
unknown <amalnazeem@gmail.com>
2019-03-28 04:51:48 -0400

    Tweaked enemy speeds to be faster

--- src/constants/enemy.js
+++ src/constants/enemy.js
@@ -1,4 +1,4 @@
-export const ENEMY_SPEED = 100
+export const ENEMY_SPEED = 200
 export const ENEMY_ROTATION_SPEED = 2 * Math.PI / 4
 export const ENEMY_DIE_TIME = 500
 export const ENEMY_WIN_TINT = 0x00ff00
