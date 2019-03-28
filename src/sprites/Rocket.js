3b4ed5297a3f64bb128e577107a56a8ed4b9f8ae
unknown <amalnazeem@gmail.com>
2019-03-28 02:51:43 -0400

    Consolidating all Rocket specific logic to Rocket file

--- src/sprites/Rocket.js
+++ src/sprites/Rocket.js
@@ -1,4 +1,4 @@
-import { WORLD_WIDTH, WORLD_HEIGHT } from '../constants'
+import { WORLD_WIDTH, WORLD_HEIGHT, BULLET_VELOCITY } from '../constants'
 /**
  * Class for Rocket like bullets that fire from the ship and remove themselves after going out of the world bounds
  * No need to overwrite contructor here same as regular Sprite
@@ -6,6 +6,20 @@ import { WORLD_WIDTH, WORLD_HEIGHT } from '../constants'
  */
 export class Rocket extends Phaser.GameObjects.Sprite {
 
+  /**
+   * Orients the rocket in the given rotation (in radians) and fires towards given coordinates
+   * @param {Number} rotation
+   * @param {Number} toX 
+   * @param {Number} toY 
+   */
+  fire(rotation, toX, toY) {
+    this.rotation = rotation
+    this.scene.physics.moveTo(this, toX, toY, BULLET_VELOCITY)
+  }
+
+  /**
+   * Update function necessary to run with the Game's Physics
+   */
   update() {
     let isOutOfBounds = this.x < 0 || this.x > WORLD_WIDTH || this.y < 0 || this.y > WORLD_HEIGHT;
     if (isOutOfBounds) {
