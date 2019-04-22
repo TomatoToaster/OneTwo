6cae39af3da76cc981d57a54bad288a824ddd292
unknown <amalnazeem@gmail.com>
2019-04-22 05:52:39 -0400

    Included instructions into OneTwo game

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { WORLD_WIDTH, WORLD_HEIGHT } from './constants'
 
 const config = {
   type: Phaser.AUTO,
+  parent: 'game-container',
   width: WORLD_WIDTH,
   height: WORLD_HEIGHT,
   physics: {
