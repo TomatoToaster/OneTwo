12c97cc27e6fc57a9a63c6d5efc4c0400b61377c
unknown <amalnazeem@gmail.com>
2019-04-22 05:55:45 -0400

    Making the game square now

--- src/constants/world.js
+++ src/constants/world.js
@@ -1,5 +1,5 @@
 export const WORLD_WIDTH = 800
-export const WORLD_HEIGHT = 600
+export const WORLD_HEIGHT = 800
 export const FACE_RIGHT = 0
 export const FACE_LEFT = Math.PI
 export const FACE_DOWN = Math.PI * 0.5
