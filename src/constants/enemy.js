005816b37a91961e7159292f62b1a71b70c1efa1
unknown <amalnazeem@gmail.com>
2019-05-14 01:40:02 -0400

    Modified enemy speed to be easier in beggining and harder as you go

--- src/constants/enemy.js
+++ src/constants/enemy.js
@@ -1,5 +1,5 @@
-export const ENEMY_SPEED = 200
-export const ENEMY_SPEED_SCALING = 0.05
+export const ENEMY_SPEED = 100
+export const ENEMY_SPEED_SCALING = 0.15
 export const ENEMY_ROTATION_SPEED = 2 * Math.PI / 4
 export const ENEMY_ROTATION_SPEED_SCALING = 0.025
 export const ENEMY_DIE_TIME = 500
